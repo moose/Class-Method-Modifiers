0fa9469ef480edcc250a2a9b181431bb5fb4b58f
Karen Etheridge <ether@cpan.org>
2023-01-16 13:26:29 -0800

    increment $VERSION after 2.14 release

--- lib/Class/Method/Modifiers.pm
+++ lib/Class/Method/Modifiers.pm
@@ -5,7 +5,7 @@ package Class::Method::Modifiers;
 # KEYWORDS: method wrap modification patch
 # vim: set ts=8 sts=4 sw=4 tw=115 et :
 
-our $VERSION = '2.14';
+our $VERSION = '2.15';
 
 use base 'Exporter';
 
